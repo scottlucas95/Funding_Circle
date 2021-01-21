a62973678b16850cf1ff3faf200fe58cc4ea3ab0
scottlucas95 <scottlucas1995@gmail.com>
2021-01-21 22:15:10 +0000

    Initial commit

---
+++ credit_score_card.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn import model_selection
+import os
+
+print()
